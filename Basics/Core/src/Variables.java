299c5ae4f1d4dc67cab92465e0049308d54a0c61
Dhruv <dhruvchitkara27@gmail.com>
2019-01-16 00:54:44 +0530

    Variables code

--- Basics/Core/src/Variables.java
+++ Basics/Core/src/Variables.java
@@ -1,5 +1,13 @@
 public class Variables {
-    public static void main(String args){
+    public static void main(String[] args){
         System.out.println("Hello");
+        int marioScore = 200;
+        int lifeUps = 50;
+
+        int finalMarioScore = 200 * 50;
+
+        System.out.println(finalMarioScore);
+
+
     }
 }
