40eff82d4da575c12a89eb8095496e3ded21b204
Dhruv <dhruvchitkara27@gmail.com>
2019-01-16 01:54:21 +0530

    Data types functionality

--- Basics/Core/PrimitiveDataTypes/src/DataTypes.java
+++ Basics/Core/PrimitiveDataTypes/src/DataTypes.java
@@ -1,2 +1,7 @@
 public class DataTypes {
+    public static void main(String[] args){
+        System.out.println("Data Types");
+        byte mByte = 127;
+        System.out.println(mByte);
+    }
 }
