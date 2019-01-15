814fcb968bda9f6780e1c2e3f523bca609b2e439
Dhruv <dhruvchitkara27@gmail.com>
2019-01-16 02:42:01 +0530

    Data types functionality

--- Basics/Core/PrimitiveDataTypes/src/DataTypes.java
+++ Basics/Core/PrimitiveDataTypes/src/DataTypes.java
@@ -1,7 +1,29 @@
 public class DataTypes {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Data Types");
-        byte mByte = 127;
-        System.out.println(mByte);
+
+
+        int mInt = 7;
+        float mFloat = 7f;
+        double mDouble = 7d;
+
+        System.out.println("Int value is : " + mInt);
+        System.out.println("Float value is : " + mFloat);
+        System.out.println("Double value is " + mDouble);
+        boolean isActive = false;
+
+        System.out.println(isActive);
+
+        char mChar = 'A';
+
+        String myValue = Integer.toHexString(mChar);
+
+        System.out.println(myValue);
+
+        System.out.println(mChar);
+
+        char mUniCode = '\u0041';
+
+        System.out.println(mUniCode);
     }
 }
