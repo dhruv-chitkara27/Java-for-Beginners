dbbbc2a34768c4b61d8c9511bb8c4c1ec8725fe2
Dhruv <dhruvchitkara27@gmail.com>
2019-01-17 00:01:44 +0530

    arithmatic operators

---
+++ Basics/Core/Arithmatic/src/Arithmatic.java
@@ -0,0 +1,26 @@
+public class Arithmatic {
+    public static void main(String[] args){
+        int number1 = 10;
+        int number2 = 20;
+        int number3 = 35;
+
+        //addition
+        System.out.println(number1 + number2 + number3);
+
+        //subtraction
+        System.out.println(number2 - number1);
+
+        //multiplication
+        System.out.println(number1 * number2);
+
+        //division
+        System.out.println(number3 / number1);
+
+        //remainder
+        System.out.println(number3 % number1);
+
+        System.out.println(number1++);
+
+        System.out.println(++number1);
+    }
+}
