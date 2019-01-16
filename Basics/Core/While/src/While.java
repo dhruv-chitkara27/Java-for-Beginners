4ac6bd858664c2b33dd8d8fd3a651c45bd35448a
Dhruv <dhruvchitkara27@gmail.com>
2019-01-17 02:04:17 +0530

    While

---
+++ Basics/Core/While/src/While.java
@@ -0,0 +1,12 @@
+public class While {
+    public static void main(String args[]) {
+        int myVariable = 1;
+
+        while(myVariable <= 100) {
+            System.out.println(myVariable);
+            myVariable++;
+        }
+
+        System.out.println("I am out of the loop");
+    }
+}
