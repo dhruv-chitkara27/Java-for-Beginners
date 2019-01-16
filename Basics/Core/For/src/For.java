00413a45f77b10d8f15fba82288590b1d00d49f2
Dhruv <dhruvchitkara27@gmail.com>
2019-01-17 02:14:06 +0530

    For loop

---
+++ Basics/Core/For/src/For.java
@@ -0,0 +1,8 @@
+public class For {
+    public static  void main(String args[]) {
+
+        for(int myVariable = 1; myVariable < 100; myVariable = myVariable + 1) {
+            System.out.println(myVariable);
+        }
+    }
+}
