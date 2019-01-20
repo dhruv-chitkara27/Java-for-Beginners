abe4b8aa4cbe4e0c88955b62ede0380371f2f5b4
Dhruv <dhruvchitkara27@gmail.com>
2019-01-20 18:34:42 +0530

    PatternA

---
+++ University/Progs/PatternA/src/PatternA.java
@@ -0,0 +1,12 @@
+public class PatternA {
+
+    public static void main(String args[]) {
+        for(int i=1; i<=8;i++) {
+            for(int j=0;j<i;j++) {
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
+        }
+    }
+}
