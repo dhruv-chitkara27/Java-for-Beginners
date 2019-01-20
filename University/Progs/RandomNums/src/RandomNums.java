a880f7b0f3b7446cae0ba4335d30e9b7763c085e
Dhruv <dhruvchitkara27@gmail.com>
2019-01-20 20:49:22 +0530

    Random Numbers

--- University/Progs/RandomNums/src/RandomNums.java
+++ University/Progs/RandomNums/src/RandomNums.java
@@ -1,5 +1,9 @@
 public class RandomNums {
     public static void main(String args[]) {
-        System.out.println( (int)(Math.random()*100));
+        for(int i=0;i<=5;i++)
+        {
+            System.out.println( (int)(Math.random()*100));
+
+        }
     }
 }
