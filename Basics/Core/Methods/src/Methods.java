c3a72216bc4456b75a388506a47f873b073a8446
Dhruv <dhruvchitkara27@gmail.com>
2019-01-21 19:38:30 +0530

    access modifiers

---
+++ Basics/Core/Methods/src/Methods.java
@@ -0,0 +1,10 @@
+public class Methods {
+
+    //public - visible to everyone
+    //private - visible only to class
+    //protected - visible to the package and all subclass
+    private int score = 5;
+    public static void main(String args[]) {
+
+    }
+}
