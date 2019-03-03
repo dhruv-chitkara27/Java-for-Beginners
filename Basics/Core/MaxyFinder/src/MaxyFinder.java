6115136f66be9b20b2885eafb05f9c3e4dba229a
Dhruv <dhruvchitkara27@gmail.com>
2019-03-03 21:18:08 +0530

    Java Basics Completed

--- Basics/Core/MaxyFinder/src/MaxyFinder.java
+++ Basics/Core/MaxyFinder/src/MaxyFinder.java
@@ -20,5 +20,13 @@ public class MaxyFinder {
     public static void main(String args[]) {
 
         System.out.printf("Max of %d, %d, %d, is: %d", 2, 4, 3, maxy(2,4,3));
+
+        Holder<String> stringValue = new Holder<>();
+        stringValue.setMyVar(new String("\nHello World"));
+        System.out.println(stringValue.getMyVar());
+
+        Holder<Integer> integerValue = new Holder<>();
+        integerValue.setMyVar(new Integer("3"));
+        System.out.println(integerValue.getMyVar());
     }
 }
